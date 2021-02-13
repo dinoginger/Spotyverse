0480cda6e859a5fc3aa4229b80a5535d46f3b35a
Marko Ruzak <marcothemars@gmail.com>
2021-02-13 17:07:20 +0200

    Top 3 genres feature done for minute overload of function. for default needs to be done and tested as well

--- Discord_API1/Service/SpotifyService.cs
+++ Discord_API1/Service/SpotifyService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using SpotifyAPI.Web;
 using Swan;
@@ -29,11 +30,20 @@ namespace Discord_API1.Service
             
             dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(data_json);
 
-            var link = data.Items[0].ExternalUrls.spotify; //АДРЕСА url ТРЕКУ
+            //var link = data.Items[0].ExternalUrls.spotify; //АДРЕСА url ТРЕКУ
             var popularity = data.Items[0].Popularity; //популярність треку
             
-            return Tuple.Create((int)popularity, link.ToString());
+            //GenreSearch
+            string artistid = data.Items[0].Album.Artists[0].Id.ToString();
+            var artist = await spotify.Artists.Get(artistid); 
+            var artistname = data.Items[0].Artists[0].Name.ToString(); 
+            string genres_string = "";
+            foreach (var genre in artist.Genres.ToArray())
+            {
+                genres_string = genres_string + "+" + genre;
+            }
             
+            return Tuple.Create((int)popularity,genres_string);
             
             }
             catch (Exception e)
@@ -41,6 +51,33 @@ namespace Discord_API1.Service
                 throw new ArgumentException($"Song \"{songName}\" was not found.");
                 
             }
+
+        }
+
+        public static string GetTopGenres(string all_genres_string)
+        {
+            string topgenres = "";
+            var genre_list = all_genres_string
+                .Split(new[] { '+' }, StringSplitOptions.RemoveEmptyEntries)
+                .GroupBy(w => w)
+                .OrderByDescending(g => g.Count());
+
+            foreach (var word in genre_list){
+                Console.WriteLine("{0}x {1}", word.Count(), word.Key);
+            }
+
+            int mm = 0;
+            foreach (var genre in genre_list )
+            {
+                if (genre.Count() >= 2 && mm < 3)
+                {
+                    topgenres = topgenres + "/" + genre.Key;
+                    mm++;
+                }
+            }
+
+            return topgenres;
+            //TODO: return string look better, do stuff with slashes
 
         }
     }
