668e73d2881a3b1e5c03be20d96614fe8a798109
Marko Ruzak <marcothemars@gmail.com>
2021-02-15 00:35:27 +0200

    Rebuild CommandHandler.cs. Now added OnCommandExecute, to make more opperations with commands.

--- Discord_API1/CommandHandler.cs
+++ Discord_API1/CommandHandler.cs
@@ -12,31 +12,50 @@ namespace Discord_API1
     
     {
         private DiscordSocketClient _client;
-        private CommandService _service;
+        private CommandService _commands;
         
         public CommandHandler(DiscordSocketClient client)
         { 
             _client = client;
-            _service = new CommandService();
+            _commands = new CommandService();
+            _commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+            // Hook the execution event
+            _commands.CommandExecuted += OnCommandExecutedAsync;
+            // Hook the command handler
             _client.MessageReceived += HandleCommandAsync;
-            _service.AddModulesAsync(Assembly.GetEntryAssembly(), null);
         }
-        private async Task HandleCommandAsync(SocketMessage message)
+        public async Task OnCommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
         {
-            var msg = message as SocketUserMessage;
-            if (msg ==null) return;
-            int argPos = 0;
-            var _context = new SocketCommandContext(_client,msg);
-            if(msg.HasCharPrefix('!',ref argPos))
-            {
+            // We have access to the information of the command executed,
+            // the context of the command, and the result returned from the
+            // execution in this event.
 
-                var result = await _service.ExecuteAsync(_context, argPos, services: null);
-                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
-                {
-                    await _context.Channel.SendMessageAsync(result.ErrorReason);
-                    
-                }
+            // We can tell the user what went wrong
+            if (!string.IsNullOrEmpty(result?.ErrorReason))
+            {
+                await context.Channel.SendMessageAsync(result.ErrorReason);
             }
+
+            // ...or even log the result (the method used should fit into
+            // your existing log handler)
+            var commandName = command.IsSpecified ? command.Value.Name : "A command";
+            Console.WriteLine($"Command {commandName} was executed at {DateTime.UtcNow}.");
+            
+        }
+        public async Task HandleCommandAsync(SocketMessage msg)
+        {
+            var message = msg as SocketUserMessage;
+            if (message == null) return;
+            int argPos = 0;
+            if (!(message.HasCharPrefix('!', ref argPos) ||
+                  message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                message.Author.IsBot)
+            {
+                return;
+            }
+            
+            var context = new SocketCommandContext(_client, message);
+            await _commands.ExecuteAsync(context, argPos, null);
         }
     }
     
