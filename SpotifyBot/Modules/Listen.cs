56d6c6d9260c17c9bab2bc9c4a0acd15f64ecdac
Marko Ruzak <marcothemars@gmail.com>
2021-03-13 15:50:19 +0200

    Fixed #10 : deleted overload and just made socketuser as optional.

--- SpotifyBot/Modules/Listen.cs
+++ SpotifyBot/Modules/Listen.cs
@@ -23,194 +23,99 @@ namespace SpotifyBot.Modules
         
         ///Default value of minutes command !listen runs, if not overloaded with minutes parameter
         private const int command_cooldown = 5;//--minutes \\\\Determines cooldown for our listen command;
-        
         private const int wait_seconds = 30; //--seconds \\\\\Period of time we wait before checking song again
 
         
-
-
-
-        [Command("listen", RunMode = RunMode.Async)]
-        //Here goes default ratelimit, in prioritized is my own.
-        //Idk why but it wont work good otherwise ¯\_(ツ)_/¯
-        [Ratelimit(1,command_cooldown,Measure.Minutes, RatelimitFlags.None)]
-        [Priority(1)] //less prioritized
-        public async Task Listenn(float minutes)
-        {
-            var embedBuilder = new EmbedBuilder();
-            SocketUser user = Context.User;
-            if (minutes <= 0.5f)
-            {
-                await Context.Channel.SendMessageAsync("Time period must be more than 0,5 min.");
-                return;
-            }
-
-            int p = (int)(minutes*60)/wait_seconds; //How many times cycle will run
-            song_data[] songData = new song_data[p];
-            int d = 0;
-            await Context.Channel.SendMessageAsync("starting...");
-            for (int i = 0; i < p; i++)
-            {
-                var activities = user.Activities;
-                foreach (var activity in activities) //Typecheckkk
-                {
-                    if (activity is SpotifyGame spot)
-                    {
-                        try
-                        {
-                            Console.WriteLine($"{d + 1} song recieved");
-                            var tuple = SpotifyService.Listen(spot.TrackTitle + " " + spot.Artists.First());
-                            songData[d].popularity = tuple.Result.Item1;
-                            songData[d].genre_string = tuple.Result.Item2;
-                            
-                            songData[d].songname = spot.TrackTitle;
-                            Console.WriteLine(spot.TrackTitle);
-                            Console.WriteLine(songData[d].genre_string);
-                            d++;
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.InnerException.Message);
-                        }
-                    }
-                }
-
-                if (i != p - 1) //For not delaying after last time
-                {
-                    await Task.Delay(wait_seconds*1000);
-                }
-            }
-            
-            
-            //Console.WriteLine("\n\n");
-            
-            var distinct_data = songData.Distinct();
-            int[] popularities = new int[distinct_data.Count()];
-            int dd = 0;
-            string distinct_genres = "";
-            foreach (var data in distinct_data)
-            {
-                Console.WriteLine(data.songname);
-                Console.WriteLine(data.genre_string);
-                popularities[dd] = data.popularity;
-                dd++;
-                distinct_genres = distinct_genres + "+" +  data.genre_string;
-            }
-
-            //Console.WriteLine($"\n\n{distinct_genres}");
-            
-            var topGenres = SpotifyService.GetTopGenres(distinct_genres);
-            try
-            {
-                Random random = new Random();
-                embedBuilder.WithAuthor($"for {user.Username}")
-                    .WithTitle($"How basic your music taste is, based on {popularities.Length} songs :")
-                    .AddField("============", $"Your playlist is {Math.Round(popularities.Average(), 1)}% basic.", false)
-                    .WithCurrentTimestamp()
-                    .WithColor(new Color(random.Next(0,255),random.Next(0,255),random.Next(0,255)));
-                if (topGenres.Length > 2)
-                {
-                    embedBuilder.AddField("============", $"Top genres are : {topGenres}", false);
-                }
-                else //Funne face when no topgenres
-                {
-                    embedBuilder.AddField("============", "^_^", false);
-                }
-                await Context.Channel.SendMessageAsync("", false, embedBuilder.Build()); 
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                
-                
-            }
-
-        }
         /// <summary>
-        /// This overload is for custom user
+        /// Listen command description
         /// </summary>
         /// <param name="minutes"></param>
         /// <param name="user"></param>
         /// <returns></returns>
+        
         [Command("listen", RunMode = RunMode.Async)]
-        [MyRatelimit(1,5,MyMeasure.Minutes, RatelimitFlags.None)] //default cooldown attr, or else it wont work
-        [Priority(2)] //prioritized
-        public async Task Listenn(float minutes, SocketUser user) //Перегруз де юзер задає скільки часу він хоче щоб його слухали
+        [MyRatelimit(1,command_cooldown,MyMeasure.Minutes, RatelimitFlags.None)]
+        public async Task Listenn(float minutes, SocketUser user = null) 
         {
-            Console.Write("SECOND OVERLOAD WITH CUSTOM USER" + user.Username); 
-            var embedBuilder = new EmbedBuilder();
-            if (minutes <= 0.5f)
+            if (user == null)
             {
-                await Context.Channel.SendMessageAsync("Time period must be more than 0,5 min.");
-                return;
+                user = Context.User;
             }
 
-            int p = (int)(minutes*60)/wait_seconds; //How many times loop will run
-            song_data[] songData = new song_data[p];
-            int d = 0;
-            string[] artist = new string[p];
-            string[] title = new string[p];
-
-            await Context.Channel.SendMessageAsync("starting...");
-            for (int i = 0; i < p; i++)
+            try
             {
-                var activities = user.Activities;
-                foreach (var activity in activities) //Typecheck....
+                Console.Write(user.Username + "\n"); //test : see which user we listen to 
+                var embedBuilder = new EmbedBuilder();
+                if (minutes <= 0.5f)
                 {
-                    if (activity is SpotifyGame spot)
+                    throw new ArgumentException("Time period must be more than 0,5 min.");
+                }
+
+                int p = (int) (minutes * 60) / wait_seconds; //How many times loop will run
+                song_data[] songData = new song_data[p];
+                int d = 0;
+                string[] artist = new string[p];
+                string[] title = new string[p];
+
+                await Context.Channel.SendMessageAsync("starting...");
+                for (int i = 0; i < p; i++)
+                {
+                    bool Spotify_exists  = false; //To determine whether first activity check contains spotify 
+                    var activities = user.Activities;
+                    foreach (var activity in activities) //Typecheck....
                     {
-                        try
+                        if (activity is SpotifyGame spot)
                         {
+                            Spotify_exists = true;
                             Console.WriteLine($"{d + 1} song recieved");
                             var tuple = SpotifyService.Listen(spot.TrackTitle + " " + spot.Artists.First());
                             songData[d].popularity = tuple.Result.Item1;
                             songData[d].genre_string = tuple.Result.Item2;
-                            
+
                             songData[d].songname = spot.TrackTitle;
                             Console.WriteLine(spot.TrackTitle);
                             Console.WriteLine(songData[d].genre_string);
                             d++;
                         }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.InnerException.Message);
-                        }
+                    }
+
+                    if (!Spotify_exists && i == 0)
+                    {
+                        throw new Exception("No spotify activity detected.");
+                    }
+
+                    if (i != p - 1) //For not delaying after last time
+                    {
+                        await Task.Delay(wait_seconds * 1000);
                     }
                 }
 
-                if (i != p - 1) //For not delaying after last time
+
+                //Console.WriteLine("\n\n");
+
+                var distinct_data = songData.Distinct();
+                int[] popularities = new int[distinct_data.Count()];
+                int dd = 0;
+                string distinct_genres = "";
+                foreach (var data in distinct_data)
                 {
-                    await Task.Delay(wait_seconds*1000);
+                    Console.WriteLine(data.songname);
+                    Console.WriteLine(data.genre_string);
+                    popularities[dd] = data.popularity;
+                    dd++;
+                    distinct_genres = distinct_genres + "+" + data.genre_string;
                 }
-            }
-            
-            
-            //Console.WriteLine("\n\n");
-            
-            var distinct_data = songData.Distinct();
-            int[] popularities = new int[distinct_data.Count()];
-            int dd = 0;
-            string distinct_genres = "";
-            foreach (var data in distinct_data)
-            {
-                Console.WriteLine(data.songname);
-                Console.WriteLine(data.genre_string);
-                popularities[dd] = data.popularity;
-                dd++;
-                distinct_genres = distinct_genres + "+" +  data.genre_string;
-            }
-            
-            //Console.WriteLine($"\n\n{distinct_genres}"); - in case you wanna see it
-            
-            var topGenres = SpotifyService.GetTopGenres(distinct_genres);
-            try
-            {
+
+                //Console.WriteLine($"\n\n{distinct_genres}"); - in case you wanna see it
+
+                var topGenres = SpotifyService.GetTopGenres(distinct_genres);
                 Random random = new Random();
                 embedBuilder.WithAuthor($"for {user.Username}")
                     .WithTitle($"How basic your music taste is, based on {popularities.Length} songs :")
-                    .AddField("============", $"Your playlist is {Math.Round(popularities.Average(), 1)}% basic.", false)
+                    .AddField("============", $"Your playlist is {Math.Round(popularities.Average(), 1)}% basic.",
+                        false)
                     .WithCurrentTimestamp()
-                    .WithColor(new Color(random.Next(0,255),random.Next(0,255),random.Next(0,255)));
+                    .WithColor(new Color(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255)));
                 if (topGenres.Length > 2)
                 {
                     embedBuilder.AddField("============", $"Top genres are : {topGenres}", false);
@@ -219,11 +124,12 @@ namespace SpotifyBot.Modules
                 {
                     embedBuilder.AddField("============", "^_^", false);
                 }
-                await Context.Channel.SendMessageAsync("", false, embedBuilder.Build()); 
+
+                await Context.Channel.SendMessageAsync("", false, embedBuilder.Build());
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                throw new Exception($"Command aborted : {e.Message}");
             }
         }
     }
