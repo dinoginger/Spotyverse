603c4ac1050b198aa85ffeaf64e8d01e6178bb7b
Marco Ruzak <marcothemars@gmail.com>
2021-06-03 18:06:44 +0300

    +vote links to <<help command

--- bot/Modules/minor-Commands.cs
+++ bot/Modules/minor-Commands.cs
@@ -8,6 +8,10 @@ namespace SpotifyBot.Modules
     public class Test : ModuleBase<SocketCommandContext>
     {
 
+        //Links to bot's pages on top.gg and discordbotlist.com
+        private const string topgg_link = "https://top.gg/bot/802544166860095489/vote";
+        private const string discordbotlist_link = "https://discordbotlist.com/bots/spotyverse/upvote";
+
         [Command("invite")]
         [Alias("inv")]
         public async Task invite()
@@ -17,6 +21,17 @@ namespace SpotifyBot.Modules
             var embed = new EmbedBuilder();
             embed.Description += $"Invite the bot to your server by [this link]({Program.invite_link})";
             embed.WithColor(new Color(125,164,120));
+            var topgg = new EmbedFieldBuilder();
+            topgg.IsInline = true;
+            topgg.Name = "We on top.gg";
+            topgg.Value = $"[vote!]({topgg_link})";
+            embed.AddField(topgg);
+            
+            var dbotlist = new EmbedFieldBuilder();
+            dbotlist.IsInline = true;
+            dbotlist.Name = "We on discordbotlist";
+            dbotlist.Value = $"[vote!]({discordbotlist_link})";
+            embed.AddField(dbotlist);
             await gencom.SendMessageAsync("", false, embed.Build());
             await gencom.CloseAsync(); 
         }
